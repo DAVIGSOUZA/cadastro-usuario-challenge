9482f6e2fafdd9d3660b7d11b76c772c04c65911
davi garcia <davi.souza@ish.com.br>
2021-12-18 15:25:32 -0300

    add register and get_user_data api endpoints

--- backend/pontotel/api.py
+++ backend/pontotel/api.py
@@ -1,71 +1,133 @@
+import sqlite3
 from flask import json, request, jsonify, Blueprint, make_response
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 import jwt
 import os
+import re
 from datetime import datetime, timedelta
 from functools import wraps
 from pontotel.db import get_db
+from .utils import dict_factory
 
 bp = Blueprint('api', __name__)
 
 
 # decorator for check JWT
 def token_required(f):
-  @wraps(f)
-  def decorated(*args, **kwargs):
-    token = None
-    
-    if 'x-access-token' in request.headers:
-      token = request.headers['x-access-token']
-    
-    if not token:
-      return jsonify({'message' : 'Token is missing'}), 401
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
 
-    try:
-      data = jwt.decode(token, os.environ.get('SECRET_KEY'))
-      user = get_db().execute(
-        'SELECT * FROM user WHERE email = ?', (data['email'],)
-      ).fetchone()
-    except:
-      return jsonify({'message' : 'Token is invalid'}), 401
-    
-    return f(user, *args, **kwargs)
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
 
-  return decorated
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, os.environ.get('SECRET_KEY'))
+            user = get_db().execute(
+                'SELECT * FROM user WHERE email = ?', (data['email'],)
+            ).fetchone()
+        except:
+            return jsonify({'message': 'Token is invalid'}), 401
+
+        return f(user, *args, **kwargs)
+
+    return decorated
 
 
 # route for login user
 @bp.route('/login', methods=['POST'])
-def login():
-  auth = request.json
-  email = auth['email']
-  password = auth['password']
-  response = 'Error: Try verify your username and password'
+def api_login():
+    auth = request.json
+    email = auth['email']
+    password = auth['password']
+    response = 'Error: Try verify your username and password'
 
-  if not auth or not email or not password:
-    return make_response(response, 401)
-  
-  user = get_db().execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
+    if not auth or not email or not password:
+        return make_response(jsonify({'message': response}), 401)
 
-  if not user:
-    return make_response(response, 401)
-  
-  if check_password_hash(user['password'], password):
-    token = jwt.encode({
-      'email': user['email'],
-      'exp': datetime.utcnow() + timedelta(minutes = 60)
-    }, os.environ.get('SECRET_KEY'))
+    user = get_db().execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
 
-    return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
+    if not user:
+        return make_response(jsonify({'message': response}), 401)
 
-  return make_response(response, 401)
+    if check_password_hash(user['password'], password):
+        token = jwt.encode({
+            'email': user['email'],
+            'exp': datetime.utcnow() + timedelta(minutes=60)
+        }, os.environ.get('SECRET_KEY'))
 
+        return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
 
+    return make_response(jsonify({'message': response}), 401)
 
 
-# api_register_user
+# route for register new user
+@bp.route('/register', methods=['POST'])
+def api_register():
+    data = request.json
+    email = data['email']
+    password = data['password']
+    name = data['name']
+    country = data['country']
+    federal_state = data['federal_state']
+    city = data['city']
+    cep = data['cep']
+    street = data['street']
+    residential_number = data['residential_number']
+    aditional_address_info = data['aditional_address_info']
+    cpf = data['cpf']
+    pis = data['pis']
 
-# api_get_user
+    db = get_db()
+    error = None
+    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    if (
+        not email or not password or not name or not country or
+        not federal_state or not city or not cep or not street or
+        not residential_number or not cpf or not pis
+    ):
+        error = 'Preencha os campos requeridos.(*)'
+        return make_response(jsonify({'message': error}), 422)
+    elif not re.fullmatch(email_regex, email):
+        error = 'Informe um email válido.'
+        return make_response(jsonify({'message': error}), 422)
+
+    if error is None:
+        try:
+            db.execute(
+                "INSERT INTO user (email, password, name, country, federal_state, city, cep, street, residential_number, aditional_address_info, cpf, pis) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (email, generate_password_hash(password), name, country, federal_state,
+                 city, cep, street, residential_number, aditional_address_info, cpf, pis)
+            )
+            db.commit()
+
+            return make_response('User registered.', 201)
+
+        except db.IntegrityError:
+            error = f"User {email} already exists."
+
+            return make_response(jsonify({'message': error}), 409)
+
+
+@bp.route('/profile', methods=['GET'])
+@token_required
+def get_user_data(user):
+    db = get_db()
+    
+    db.row_factory = dict_factory
+    cursor = db.cursor()
+    user_data = cursor.execute(
+        'SELECT * FROM user WHERE email = ?', (user['email'],)
+    ).fetchone()
+
+    print(user_data)
+
+    return jsonify(user_data)
+
 
 # api_update_user
 
